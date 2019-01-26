7ed4a6d6c414bf68ce9d09fad83d7a4e4881f20a
sky <2449068617@qq.com>
2019-01-26 11:21:17 +0800

    界面更改完成

---
+++ utils.h
@@ -0,0 +1,17 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <QObject>
+
+class Utils : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Utils(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // UTILS_H
